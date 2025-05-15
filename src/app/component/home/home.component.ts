4e785dab9fc695eedf1fe2b8de76e63a8df47b0e
YogeshTiwari2000 <yogeshtiwari.b.007@gmail.com>
2025-05-15 17:22:22 +0530

    portfolio

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -1,16 +1,57 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {FormsModule} from '@angular/forms';
+// import {MatInputModule} from '@angular/material/input';
+// import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormBuilder, FormsModule, ReactiveFormsModule, Validators,  } from '@angular/forms';
+import emailjs from '@emailjs/browser';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [FormsModule, MatFormFieldModule, MatInputModule],
+  imports: [FormsModule, CommonModule , ReactiveFormsModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  private fb = inject(FormBuilder);
+  private http = inject(HttpClient);
+  form = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    name: ['',],
+    phone: ['',],
+    message: ['', Validators.required],
+  });
+  status: string = '';
+  submitting = false;
+  showNavbar = false;
+
+  toggleNavbar(){
+this.showNavbar = !this.showNavbar;
+  }
+
+  onSubmit() {
+    if (this.form.invalid) return;
+
+    this.submitting = true;
+    const formData = this.form.value;
+
+    const headers = new HttpHeaders({ 'Accept': 'application/json' });
+
+    this.http.post('https://formspree.io/f/mldbaapr', formData, { headers }).subscribe({
+      next: () => {
+        this.status = 'success!';
+        this.form.reset();
+        this.submitting = false;
+      },
+      error: () => {
+        this.status = ' There was an error. Please try again.';
+        this.submitting = false;
+      },
+    });
+  }
+
+ 
 
 }
